610dd2abf8467b473549292e8cb45c4629e547ad
Bobby Naughton <bnaughton22@gmail.com>
2024-02-01 16:18:44 -0500

    Updates

---
+++ precache-manifest.e58f55b579b60cdaf7d0b4688d220906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86587d9a5b89cd3e765e55fc0c8a6590",
+    "url": "/MySite/index.html"
+  },
+  {
+    "revision": "be059202d99d7cd6e6a5",
+    "url": "/MySite/static/css/2.71fe5322.chunk.css"
+  },
+  {
+    "revision": "17657922460e80a02a30",
+    "url": "/MySite/static/css/main.6ac21dd4.chunk.css"
+  },
+  {
+    "revision": "be059202d99d7cd6e6a5",
+    "url": "/MySite/static/js/2.5d693b1c.chunk.js"
+  },
+  {
+    "revision": "17657922460e80a02a30",
+    "url": "/MySite/static/js/main.56170242.chunk.js"
+  },
+  {
+    "revision": "d1b7212cc3c7345bea06",
+    "url": "/MySite/static/js/runtime-main.d692b148.js"
+  }
+]);
